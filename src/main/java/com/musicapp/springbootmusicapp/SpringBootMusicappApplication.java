1386f46f992c439c9bd89f328be9abc81310abd7
eplalba <balint.a.palotai@ericsson.com>
2023-10-05 15:44:57 +0200

    Spring app created

---
+++ src/main/java/com/musicapp/springbootmusicapp/SpringBootMusicappApplication.java
@@ -0,0 +1,13 @@
+package com.musicapp.springbootmusicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMusicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMusicappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicapp/springbootmusicapp/SpringBootMusicappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicapp.springbootmusicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMusicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
