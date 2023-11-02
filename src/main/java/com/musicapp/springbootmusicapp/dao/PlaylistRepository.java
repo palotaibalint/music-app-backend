4a25ccea1fb3bfde993b2cdeb64f7295d35a134b
palotaibalint <p.balint0301@gmail.com>
2023-11-02 11:43:52 +0100

    Added repository

---
+++ src/main/java/com/musicapp/springbootmusicapp/dao/PlaylistRepository.java
@@ -0,0 +1,12 @@
+package com.musicapp.springbootmusicapp.dao;
+
+import com.musicapp.springbootmusicapp.entity.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    List<Playlist> findAll();
+}
